8bb10b0e53a6e35f88928b1fd2efca83d92c1395
nonalaz <nonalaz@gmail.com>
2016-06-17 11:34:01 +0400

    Looping a triangle

--- eloquent-javascript/el_js_ex1.js
+++ eloquent-javascript/el_js_ex1.js
@@ -1,19 +1,9 @@
-/* // solution 1
+/**  Looping a triangle  **/
+
 var mountain = "";
 for(var i=0; i<7; i++){
 	mountain +="#";
 	console.log(mountain);
-}
-*/
+};
 
-//solution 2
 
-var m="";
-for(var j=0; j<8; j++){
-for(var i=1; i<=4;i++){
-	if(i%2==1) m +="# ";
-	m +=" #";
-	}
-	console.log(m);
-	m="";
-}
