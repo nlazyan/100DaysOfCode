df5b6788ef4f899735d6ce52b19f60fe1046f4fe
nonalaz <nonalaz@gmail.com>
2016-06-17 13:52:23 +0400

    added solution ofFizzBuzz

--- eloquent-javascript/el_js_ex1.js
+++ eloquent-javascript/el_js_ex1.js
@@ -6,4 +6,11 @@ for(var i=0; i<7; i++){
 	console.log(mountain);
 };
 
-
+var n=21;
+for(var i=1; i<=n; i++ ){
+	var result=i;
+	if(i%3==0) result="Fizz";
+	else if(i%5==0&&i%3!==0) result="Buzz";
+	else if(i%3==0&&i%5==0) result="FizzBuzz";
+	console.log(result);
+};
