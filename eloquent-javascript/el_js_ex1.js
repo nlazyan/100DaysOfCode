e49c29db68f739ab53ddc5829ebe3ce1b774f686
nonalaz <nonalaz@gmail.com>
2016-06-17 14:01:03 +0400

    added solution of Chess board

--- eloquent-javascript/el_js_ex1.js
+++ eloquent-javascript/el_js_ex1.js
@@ -6,11 +6,31 @@ for(var i=0; i<7; i++){
 	console.log(mountain);
 };
 
-var n=21;
+
+
+/** FizzBuzz  **/
+
+var n=100;
 for(var i=1; i<=n; i++ ){
 	var result=i;
-	if(i%3==0) result="Fizz";
-	else if(i%5==0&&i%3!==0) result="Buzz";
-	else if(i%3==0&&i%5==0) result="FizzBuzz";
+	if(i%3==0&&i%5==0) {result="FizzBuzz";}
+	else if(i%5==0&&i%3!==0){ result="Buzz";}
+	else if(i%3==0) {result="Fizz";}
 	console.log(result);
 };
+
+
+/**  Chess Board  **/
+
+var size=8;
+for(var j=1; j<=size; j++){
+	var char="";
+	for(var i=1; i<=size; i++){
+	if((j+i)%2==0) char +=" ";
+	else char +="#";
+};
+console.log(char);
+}
+
+
+
